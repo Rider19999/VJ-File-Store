1bf63ec0f91434a3369be6049797a0021fa2c5f2
Rider19999 <140139292+Rider19999@users.noreply.github.com>
2024-06-22 17:30:44 +0530

    Update Script.py
    Yy

--- Script.py
+++ Script.py
@@ -49,11 +49,11 @@ Cᴜʀʀᴇɴᴛ Sʜᴏʀᴛᴇɴᴇʀ API:</b> `{shortener_api}`"""
 
 📚 ʟɪʙʀᴀʀʏ: <a href=https://docs.pyrogram.org>𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦</a>
 
-🧑🏻‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ: <a href=https://t.me/Goitachi>Owner</a>
+🧑🏻‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ: <a href=https://t.me/Goitachi>×͜×ㅤɢᴏɪᴛᴀᴄʜɪ࿐</a>
 
-👥 Movie ɢʀᴏᴜᴘ: <a href=https://t.me/Mov_Group>Movies𝐭</a>
+👥 Movie ɢʀᴏᴜᴘ: <a href=https://t.me/Mov_Group>—͟͞͞★ᴊᴏɪɴ ɴᴏᴡㅤ✓</a>
 
-📢 ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ: <a href=https://t.me/Fizzy_Botz>Fizzy Botz</a></b>
+📢 ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ: <a href=https://t.me/Fizzy_Botz>¢==[]:::ꜰɪᴢᴢʏ ʙᴏᴛᴢ::::></a></b>
 """
 
     CABOUT_TXT = """<b>ʜɪ ɪ ᴀᴍ ᴘᴇʀᴍᴀɴᴇɴᴛ ғɪʟᴇ sᴛᴏʀᴇ ʙᴏᴛ ᴡɪᴛʜ ᴄʟᴏɴᴇ ғᴇᴀᴛᴜʀᴇ + ᴄᴜsᴛᴏᴍ ᴜʀʟ sʜᴏʀᴛɴᴇʀ ɪᴛ ᴍᴇᴀɴs ᴀɴʏ ᴜsᴇʀ ᴄᴀɴ sᴇᴛ ʜɪs ᴜʀʟ sʜᴏʀᴛɴᴇʀ ᴀɴᴅ + ᴀᴜᴛᴏ ᴅᴇʟᴇᴛᴇ.
